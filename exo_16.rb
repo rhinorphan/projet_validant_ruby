5c606bc5a11e61b8976a0974c23a11b66fe3d7de
Axel Tarifa <axel.tarifa@gmail.com>
2021-04-13 18:16:53 +0200

    fifth commit // correction string exo_16.rb

--- exo_16.rb
+++ exo_16.rb
@@ -1,4 +1,4 @@
-puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+puts "Salut, bienvenue dans ma super pyramide ! Choisis un nombre d'étages entre 1 et 25."
 print "> "
 floors_input = gets.to_i
 for i in 1..floors_input 
